67ba9cea63b24c99ac32fccf1bded820b7ca0e2a
dimas3696 <dmitriyval4uk@gmail.com>
2018-03-09 10:25:45 +0200

    Add Doubly-connected list

---
+++ Doubly-connected_list/Doubly-connected_list/DoublyNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doubly_connected_list
+{
+    class DoublyNode<T>
+    {
+        public DoublyNode(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public DoublyNode<T> Next { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+    }
+}
